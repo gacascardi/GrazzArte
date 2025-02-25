8f8c0193f6d98490d3fccd93d2e4f24e2856faab
Grazielly <gacascardi@outlook.com>
2025-02-25 14:18:50 +0000

    update files

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/backstop.jpg";
+import image from "../images/blackground.png"; // Ensure this path is correct
 
 const imageAltText = "abstract background";
 
